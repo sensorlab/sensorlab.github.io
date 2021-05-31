99388abfb97fbe2b9522d2e394108d0745f0df66
Gregor Cerar <grega90@gmail.com>
2021-05-31 23:21:28 +0200

    Update. Solve #1

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
       let els = JSON.parse(content).htmlElements;
-      return els.tags.concat(els.tags, els.ids, els.classes);
+      return els.tags.concat(els.classes, els.ids);
   }
 });
 
